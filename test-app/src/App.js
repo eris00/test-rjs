2d82a1cad68654ae4c9c2b671b0bb59d077766b2
eris00 <erissutkovic00gmail.com>
2022-08-08 21:19:49 +0200

    commited

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Components/Home';
 import './App.css';
 import Scan from "./Components/Scan";
@@ -6,13 +6,44 @@ import About from "./Components/About";
 import Product from "./Components/Product";
 import Unavailable from "./Components/Unavailable";
 import ErrorPage from "./Components/ErrorPage";
+import { useState, useEffect } from 'react'
+import api from "./api/post";
 
 function App() {
+
+  const [queryId, setQueryId] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get(`/products/${queryId}`);
+        setProducts(response.data)
+        // console.log(response.data)
+      } catch(err) {
+
+          console.log(err.message)
+        
+      } 
+    }
+
+    fetchPosts();
+  }, [queryId])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQueryId(e.target.value);
+  }
+
+
+
   return (
     <Router>
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/scan" element={<Scan />} />
+        <Route path="/scan" element={<Scan handleSubmit={handleSubmit} queryId={queryId} setQueryId={setQueryId} />} />
         <Route path="/product" element={<Product />} />
         <Route path="/about" element={<About />} />
         <Route path="/unavailable" element={<Unavailable />} />
--- test-app/src/Components/Scan.js
+++ test-app/src/Components/Scan.js
@@ -3,18 +3,30 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(e.target)
-}
 
-function Scan() {
+function Scan({handleSubmit, queryId, setQueryId}) {
+
+
+
   return (
     <Box sx={{ width:"100%", marginTop:"60%", marginLeft:"5%"}}>
-      <form onSubmit={handleSubmit}>
-        <input style={{padding:"20px", width:"60%"}}/>
-        <input type="submit" value="Submit" style={{padding:"20px", width:"20%"}} />
-      </form>
+      
+    <form onSubmit={handleSubmit}>
+    <TextField 
+        sx={{ width: '50%', bgcolor: '#F4F4F4', margin:'15px' }}
+        id="outlined-search"
+        type='search'
+        label="Search" 
+        variant="filled" 
+        margin="normal"
+        onChange={(e) => {setQueryId(e.target.value)}}
+        value={queryId}
+        InputProps={{endAdornment:          
+          <Button type="submit"> OK </Button>
+        }}
+    />
+    </form>
+
     </Box>
   )
 }
